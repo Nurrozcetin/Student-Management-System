85c029f8baa4c04fde492504a790f92e6072714a
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2023-07-29 23:59:35 +0300

    payment

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         student s1 = new student();
+        s1.enroll();
+        s1.pay(500);
     }
 }
--- src/student.java
+++ src/student.java
@@ -5,7 +5,7 @@ public class student {
     private String lastName;
     private String year;
     private String studentId;
-    private String cources;
+    private String cources = null;
     private int balance;
     private static int costOfCources = 600;
     private static int id = 100;
@@ -32,5 +32,37 @@ public class student {
     // generate an id
     private void setstudentId() {
         this.studentId = year + "" + id;
+    }
+
+    // enroll courses
+    public void enroll() {
+        do {
+            System.out.println("Enter course to enroll: (Quit for Q) ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q") && (!course.equals("q"))) {
+                cources = cources + "\n" + course;
+                balance = balance + costOfCources;
+            } else {
+                System.out.println("BREAK");
+                break;
+            }
+        } while (1 != 0);
+        System.out.println("Enrolled this courses: " + cources);
+        System.out.println("Balance: " + balance);
+    }
+
+    public void showBalance() {
+        System.out.println("Your balance is: " + balance);
+    }
+
+    public void pay() {
+        showBalance();
+        System.out.println("Enter your payment ");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        balance = balance - payment;
+        System.out.println("Your payment " + payment);
+        showBalance();
     }
 }
